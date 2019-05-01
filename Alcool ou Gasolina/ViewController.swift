e9e5e22793f0664f475929903eb76a95ddabdf90
Luis Fernando Pasquinelli Amaral de Abreu <luispasquinelli@hotmail.com>
2019-05-01 02:52:02 -0300

    projeto finalizado

--- Alcool ou Gasolina/ViewController.swift
+++ Alcool ou Gasolina/ViewController.swift
@@ -10,6 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var precoAlcool: UITextField!
+    @IBOutlet weak var precoGasolina: UITextField!
+    @IBOutlet weak var textoResultado: UILabel!
+    
+    @IBAction func botaoCalcular(_ sender: Any) {
+        if let precoAlcool = precoAlcool.text{
+            if let precoGasolina = precoGasolina.text{
+               let validaCampos = self.validarCampos(precoAlcool: precoAlcool, precoGasolina: precoGasolina)
+                if validaCampos {
+                    self.calcularMelhorPreco(precoAlcool: precoAlcool, precoGasolina: precoGasolina)
+                } else {
+                    textoResultado.text = "Digite agora!"
+                }
+            }
+        }
+    }
+    
+    func calcularMelhorPreco(precoAlcool: String, precoGasolina: String){
+        if let nA = Double(precoAlcool){
+            if let nG = Double(precoGasolina){
+                let resultadoPreco = nA/nG
+                if resultadoPreco >= 0.7{
+                    self.textoResultado.text = "Ponha Gasolina!"
+                } else {self.textoResultado.text = "Ponha Álcool!"}
+            }
+        }
+    }
+    
+    func validarCampos(precoAlcool: String, precoGasolina: String) -> Bool{
+        var camposValidados = true
+        
+        if precoAlcool.isEmpty || precoGasolina.isEmpty {
+            camposValidados = false
+        }
+        return camposValidados
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
